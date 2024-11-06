ce585dcdd73e35331118cbd362b3741082c58f9e
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-06 19:05:20 +0530

    handler_mouse_inputs: Modified the file to handle mouse inputs

--- src/main/java/Inputs/MouseInputs.java
+++ src/main/java/Inputs/MouseInputs.java
@@ -1,4 +1,50 @@
 package Inputs;
 
-public class MouseInputs {
+import org.example.GamePanel;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MouseInputs implements MouseListener, MouseMotionListener {
+    // MouseListener methods
+    private GamePanel gamePanel;
+    public MouseInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // Code to handle mouse click event
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // Code to handle mouse press event
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // Code to handle mouse release event
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // Code to handle mouse entering a component
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // Code to handle mouse exiting a component
+    }
+
+    // MouseMotionListener methods
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // Code to handle mouse drag event
+    }
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // Code to handle mouse movement
+        gamePanel.setRectPos(e.getX(), e.getY());
+    }
 }
