73c8f6ef8f58ebe4f95a98f8005955559add94d9
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-06 14:54:05 +0530

    modified_game_window: Modified the Game window to include the game panel

--- src/main/java/org/example/GameWindow.java
+++ src/main/java/org/example/GameWindow.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import javax.swing.*;
+
 public class GameWindow {
+    private JFrame jFrame;
+    public GameWindow(GamePanel gamePanel) {
+        jFrame = new JFrame();
+        jFrame.setSize(400, 400);
+        jFrame.add(gamePanel);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
