a796572a8c365875a19d936c3ed50a6829575469
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-06 16:28:33 +0530

    listen_keyboard_file: Added constructor for listening to keyboard inputs from file

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import Inputs.KeyboardsInputs;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
     public GamePanel() {
-
+        addKeyListener(new KeyboardsInputs());
     }
 
     public void paintComponent(Graphics g) {
