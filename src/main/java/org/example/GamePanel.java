0bcfe5a24b9ca4fad932a0c7b3b79c4f560515b8
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-06 19:04:12 +0530

    feat(direction_input): Added the direction change function in the panel

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -1,19 +1,41 @@
 package org.example;
 
 import Inputs.KeyboardsInputs;
+import Inputs.MouseInputs;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyEvent;
+        import java.awt.*;
+        import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private int xDelta = 100, yDelta = 100;
     public GamePanel() {
-        addKeyListener(new KeyboardsInputs());
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardsInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+        repaint();
+    }
+
+    public void setRectPos(int x, int y) {
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(100, 100, 200, 50);
+        g.fillRect(xDelta,  yDelta, 200, 50);
     }
 }
