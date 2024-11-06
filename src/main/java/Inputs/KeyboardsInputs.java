9d304474c8f4c64088a23fc89e00d7af072b754c
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-06 16:26:35 +0530

    learning_keypress: Learned how to handle key press

--- src/main/java/Inputs/KeyboardsInputs.java
+++ src/main/java/Inputs/KeyboardsInputs.java
@@ -1,4 +1,36 @@
 package Inputs;
 
-public class KeyboardsInputs {
+import com.sun.security.jgss.GSSUtil;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardsInputs implements KeyListener {
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                System.out.println("Its double You");
+                break;
+            case KeyEvent.VK_A:
+                break;
+            case KeyEvent.VK_S:
+                break;
+            case KeyEvent.VK_D:
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Optional: handle key release events
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Optional: handle key typed events
+
+    }
 }
