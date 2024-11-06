2f5cdcbb43c370d157904595055dc11f49564104
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-06 19:04:50 +0530

    keyboard_inputs: Modified the file for handling direction change with keyboard

--- src/main/java/Inputs/KeyboardsInputs.java
+++ src/main/java/Inputs/KeyboardsInputs.java
@@ -1,6 +1,8 @@
 package Inputs;
 
 import com.sun.security.jgss.GSSUtil;
+import org.example.Game;
+import org.example.GamePanel;
 
 import javax.swing.*;
 import java.awt.event.KeyEvent;
@@ -8,17 +10,24 @@ import java.awt.event.KeyListener;
 
 public class KeyboardsInputs implements KeyListener {
 
+    private GamePanel gamePanel;
+    public KeyboardsInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
     @Override
     public void keyPressed(KeyEvent e) {
         switch(e.getKeyCode()) {
             case KeyEvent.VK_W:
-                System.out.println("Its double You");
+                gamePanel.changeYDelta(-5);
                 break;
             case KeyEvent.VK_A:
+                gamePanel.changeXDelta(-5);
                 break;
             case KeyEvent.VK_S:
+                gamePanel.changeYDelta(+5);
                 break;
             case KeyEvent.VK_D:
+                gamePanel.changeXDelta(+5);
                 break;
         }
     }
