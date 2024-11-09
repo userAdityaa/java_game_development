55ba2305bd8ff2ed412e98816d114137fe59b5b0
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-10 04:22:07 +0530

    game_loop: Added the game loop for testing FPS

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,11 +1,33 @@
 package org.example;
 
-public class Game {
+public class Game implements Runnable{
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    public Thread gameThread;
+    private final int FPS_SET = 120;
     public Game() {
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
+    }
+
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        while(true) {
+            now = System.nanoTime();
+            if(now - lastFrame >= timePerFrame) {
+                gamePanel.repaint();
+                lastFrame = now;
+            }
+        }
     }
 }
