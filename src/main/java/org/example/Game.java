d318acb1533dabe2090ada35d6453fd4cd3d46d2
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-10 15:43:51 +0530

    frame_include_game: Included the lastCheck and frames for the game file

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -22,11 +22,19 @@ public class Game implements Runnable{
         double timePerFrame = 1000000000.0 / FPS_SET;
         long lastFrame = System.nanoTime();
         long now = System.nanoTime();
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
         while(true) {
             now = System.nanoTime();
             if(now - lastFrame >= timePerFrame) {
                 gamePanel.repaint();
                 lastFrame = now;
+                frames++;
+            }
+            if(System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
             }
         }
     }
